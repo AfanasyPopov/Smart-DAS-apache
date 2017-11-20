0786a8f1750567acff5c84b6f0c4ce1ff10faef2
Afanasy Popov <afanasy.popov@gmail.com>
2017-11-20 19:10:35 +0300

    First commit
    add first source file set

---
+++ www/js/controllers.js
@@ -0,0 +1,188 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  // Form data for the login modal
+  var username = window.localStorage.getItem("username");
+  var password = window.localStorage.getItem("password");
+  var url = window.localStorage.getItem("url");
+  if (username&&password&&url) {
+      $scope.loginData={'username': username, 'password': password, 'url': url};
+  } else {
+      $scope.loginData= {};
+  } 
+  //Init localStorageImgArray for image-data storing (documentId:string, fullPathName:string, uploadStatus:boolean, barcodeId)
+  var localStorageImgArray = window.localStorage.getItem("localStorageImgArray");
+  if (localStorageImgArray) {
+      $scope.localStorageImgArray=JSON.parse(localStorage.getItem("localStorageImgArray"));;
+  } else {
+      $scope.localStorageImgArray= {};
+  } 
+
+  
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal 
+  $scope.login = function() {
+    $scope.modal.show();
+    
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+  window.localStorage.setItem('username', $scope.loginData["username"]);
+  window.localStorage.setItem('password', $scope.loginData["password"]);
+  window.localStorage.setItem('url', $scope.loginData["url"]);
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) { // ----------------------------------- ----------------------------------- -----------------------------------
+   
+  $scope.playlists = [
+    { title: 'Заявки на Склад: №ФР-0001', id: 1 },
+    { title: 'Заявки на Склад: №ФР-0002', id: 2 },
+    { title: 'Заявки на Склад: №ФР-0003', id: 3 }
+  ];
+})
+.controller('PhotoSearchCtrl', function($scope, Cities, $ionicPopup, $ionicLoading) { // ----------------------------------- -----------------------------------
+
+    $scope.shouldShowDelete = false;
+    $scope.shouldShowReorder = false;
+    $scope.listCanSwipe = true;
+    $scope.docLists = [];
+    var i;
+    for (i = 0; i < 10; i++) {
+     $scope.docLists.push ({title: "Заявка на Склад №"+i,description:i*1.21+" м3", docId: i, fullPathName:"img/ionic.png" });
+     for (k=0;k<localStorageImgArray.length;k++){
+        if(localStorageImgArray[k].documentId=docLists[i].title){
+            docLists[i].fullPathName = localStorageImgArray[k].fullPathName;
+        } else {
+            alert(localStorageImgArray[k].fullPathName);
+        };
+     }
+    };
+
+
+    //Show Alert  -----------------------------------
+    $scope.showAlert = function(title, text) {
+		$ionicPopup.alert({
+			title: title,
+			template: text
+		});
+	};
+    $scope.show = function() {
+    $ionicLoading.show({
+      template: 'Loading...',
+      duration: 1000
+    }).then(function(){
+       console.log("The loading indicator is now displayed");
+    });
+    };
+    $scope.hide = function(){
+        $ionicLoading.hide().then(function(){
+           console.log("The loading indicator is now hidden");
+        });
+    };
+    //Catrure photos from Camera -----------------------------------
+    $scope.capturePicture = function(itemImgId){
+       // navigator.vibrate(50);
+        itemID = itemImgId;
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,destinationType: Camera.DestinationType.FILE_URI });
+    };
+    function onSuccess(imageURI) {
+        //alert(imageURI);
+        console.log("debug() function is executed!");
+        var image = document.getElementById(itemID);
+        image.src = imageURI;
+        localStorageImgArrayAddOrReplace(itemID,imageURI);
+        alert(localStorageImgArray.length);
+
+
+    };
+    
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    };
+    
+    //Accept Good  -----------------------------------
+    $scope.acceptGood = function(itemId){
+        navigator.vibrate(100);
+    };
+    //Accept Bad  -----------------------------------
+    $scope.acceptBad = function (itemId){
+        navigator.vibrate([100]);  
+        setTimeout (function(){navigator.vibrate(100)}, 500);
+    };
+  
+})
+
+.controller('PhotoSearchDetailCtrl', function($scope, $http, $stateParams, $ionicPopup) { //  ----------------------------------- ----------------------------------
+    $scope.data = {};
+	$scope.id = $stateParams.id;
+	$scope.showAlert = function(title, text) {
+		$ionicPopup.alert({
+			title: title,
+			template: text
+		});
+	};
+	$scope.refresh = function() {
+		$http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id='+$scope.id)
+		.success(function(data, status, headers, config){
+			$scope.data = data;
+			$scope.$broadcast('scroll.refreshComplete');
+		})
+		.error(function(data, status, headers, config){
+			$scope.showAlert(status, data);
+			$scope.$broadcast('scroll.refreshComplete');
+		});
+	};
+	$scope.refresh();
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) { // ----------------------------------- ----------------------------------- -----------------------------------
+});
+
+
+//glogal Function area  ----------------------------------- ----------------------------------- ----------------------------------- -----------------------------------
+
+function localStorageImgArrayAddOrReplace(itemID,imageURI){
+    var flag= false;
+    console.log ('locStor');
+    for (var ii=0;localStorageImgArray.length;ii++){
+        if (localStorageImgArray[ii].documentId=itemID){
+            localStorageImgArray[ii].fullPathName = imageURI;
+            localStorageImgArray[ii].uploadStatus="false";       
+            flag = true;
+        };    
+    };
+    if (!flag) {
+        localStorageImgArray.push({"documentId":itemID, "fullPathName":imageURI, "uploadStatus":"false", "barcodeId":""})
+    }
+    localStorage.setItem("localStorageImgArray", JSON.stringify(localStorageImgArray));
+}
+
+// start image capture
+function fail(msg) {
+    alert("moveTo fail");
+}
---
+++ www/js/smartdas.js
@@ -0,0 +1,183 @@
+/**Afanfsy's part of code
+ * 
+ */
+//Put event listeners into place
+//alert("SmartDAS script is loaded.");
+var isCamInitDone = false;
+var canvas,context, video;
+var front = true;
+var mediaConfig ;
+
+function camInit(mediaConfig) {  // Camera initialization
+    isCamInitDone = true;
+	//Grab elements, create settings, etc.
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var video = document.getElementById('video');
+	
+	var canvas_parent_k=1;
+	var doc_1s= "not found";
+	//var mediaConfig = {video: {width: {exact: 640}, height: {exact: 480}}};
+	var errBack = function(e) {
+	  	console.log('An error has occurred!', e)
+	  };
+
+		// Put video listeners into place
+	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+	      navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
+	          window.stream = stream;
+	          video.src = window.URL.createObjectURL(stream);
+	          video.play();
+	      });
+	  }
+
+	  /* Legacy code below! */
+	  else if(navigator.getUserMedia) { // Standard
+			navigator.getUserMedia(mediaConfig, function(stream) {
+				window.stream = stream;
+				video.src = stream;
+				video.play();
+			}, errBack);
+		} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+			navigator.webkitGetUserMedia(mediaConfig, function(stream){
+				window.stream = stream;
+				video.src = window.webkitURL.createObjectURL(stream);
+				video.play();
+			}, errBack);
+		} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+			navigator.mozGetUserMedia(mediaConfig, function(stream){
+				window.stream = stream;
+				video.src = window.URL.createObjectURL(stream);
+				video.play();
+			}, errBack);
+		}
+		window.myConsole.value = window.myConsole.value+"camInit: done successfull."+"\n";
+};
+function makeShot(){  // Make capture from Video element
+	  canvasInit();
+	  video = document.getElementById('video');
+	  canvas = document.getElementById('canvas');
+	  context = canvas.getContext('2d');
+	  doc_1s="(not found)";
+	  
+	  var scl=1, sx =0, sy =0, dx=0, dy=0, k=1;
+	  var isLandscape =true;
+	  if (video.videoWidth>=video.videoHeight){
+		  	  isLandscape=true;
+		  	  k=video.videoHeight/video.videoWidth;
+		  	  dWidth = video.videoWidth;
+		  	  dHeight = dWidth*k;
+		  } else{
+			  isLandscape=false;
+		  	  k=video.videoHeight/video.videoWidth;
+		  	  dWidth = video.videoWidth;
+		  	  dHeight = dWidth*k;
+		  };
+	  dx = sx;
+	  dy = sy;
+	  //context.setTransform(1, 0, 0, 1, 0, 0);
+	  //context.scale(1,1);
+	  context.drawImage(video,sx,sy,video.videoWidth, video.videoHeight,dx,dy,dWidth,dHeight);
+	  context.font = "15px arial bold";
+	  context.strokeStyle = "white";
+	  context.strokeText("Документ: "+doc_1s+";",5,22);
+	  context.fillStyle = "blue";  // show parameters
+	  context.fillText("Документ: "+doc_1s+";",5,22);
+
+	  //context.fillText("VideoWH: "+video.videoWidth+"x"+video.videoHeight+";",5,22);
+	  //context.fillText("CanvasWH: "+canvas.clientWidth+"x"+canvas.clientHeight+";",5,42);
+	  //context.fillText("DestWH: "+dWidth+"x"+dHeight+";",5,62);
+	  //context.drawImage(video, 0, 0, parseInt($(video).css("width").split('px')[0]),parseInt($(video).css("height").split('px')[0]) );
+}; 
+function canvasInit(){  //Canvas initializine : change propertie Height, Width, etc.
+	var canvas = document.getElementById('canvas');
+	var video = document.getElementById('video');
+	var canvas_parent_k=1;
+	var video_k=video.videoHeight/video.videoWidth;
+	var canvas_parent = document.getElementById(canvas.parentElement.id);
+	//< To solve: witch size need to be iscribed
+	var height_k = canvas_parent.scrollHeight/canvas_parent.clientHeight;
+	var width_k = canvas_parent.scrollWidth/canvas_parent.clientWidth;
+	//> To solve: witch size need to be iscribed
+	if (width_k<=height_k) {
+		console.log ('var 1');
+		canvas.style.width =String(Math.round(canvas_parent.scrollWidth*width_k)-2)+'px';
+		canvas.width=video.videoWidth;
+		canvas.style.height =String(Math.round(canvas_parent.scrollWidth*width_k*video_k)-2)+'px';
+		canvas.height=video.videoHeight;
+	} else {
+		console.log ('var 2');
+		canvas.style.width =String(Math.round(canvas_parent.scrollHeight*height_k)-2)+'px';
+		canvas.width=video.videoWidth;
+		canvas.style.height =String(Math.round(canvas_parent.scrollHeight*height_k*video_k)-2)+'px';
+		canvas.height=video.videoHeight;
+	};
+	//window.myConsole.value=myConsole.value+"WH_k: "+width_k+";"+height_k+";"+"\n";
+	//window.myConsole.value=myConsole.value+"parentWH: "+canvas_parent.scrollWidth+"x"+canvas_parent.scrollHeight+";"+"\n";
+	window.myConsole.value=myConsole.value+"VideoWH: "+video.videoWidth+"x"+video.videoHeight+";"+"\n";
+};
+function readBarcode(){
+
+};
+function switchCam(){
+	var cameras=[];
+	var video = document.getElementById('video');
+	if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+		  console.log("enumerateDevices() not supported.");
+		  return;
+	}
+	window.myConsole.value  = "Switch start: "+ true +"\n";
+	if (window.stream) {
+	    video.src = null;
+	    window.stream.getTracks().forEach(function(track){
+	      track.stop();
+	    })
+  	};
+	// List cameras and microphones.
+	navigator.mediaDevices.enumerateDevices().then(function(devices) {
+	  	devices
+	.forEach(function(device) {
+		  	if (device.kind =="videoinput"){
+		    console.log(device.kind + ": " + device.label +" id = " + device.deviceId);
+		   	cameras.push({"devId":device.deviceId,"devLbl": device.label}); 
+		   	window.myConsole.value=window.myConsole.value+ device.kind + ": " + device.label+"\n";
+		   }
+	  	});
+		front = (cameras.length>1? !front: true);
+		var constraints = {};
+        constraints.video = {
+        	mandatory: {
+                           minWidth: 1024,
+                           minHeight: 768,
+                        },
+            optional: [
+                       { sourceId: (front? cameras[0].devId : cameras[1].devId)}
+                      ]
+        };
+		//mediaConfig = { video: { sourceId: (front? cameras[0] : cameras[1]) } };
+		camInit(constraints);
+		window.myConsole.value=window.myConsole.value+"Selected device: "+ (front? cameras[0].devLbl : cameras[1].devLbl)+"\n";	
+	})
+	.catch(function(err) {
+	  console.log(err.name + ": " + err.message);
+	});
+
+}
+//additional functions fron examples
+function setSelectionRange(input, selectionStart, selectionEnd) {
+  if (input.setSelectionRange) {
+    input.focus();
+    input.setSelectionRange(selectionStart, selectionEnd);
+  }
+  else if (input.createTextRange) {
+    var range = input.createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', selectionEnd);
+    range.moveStart('character', selectionStart);
+    range.select();
+  }
+
+};
+function setCaretToPos (input, pos) {
+  setSelectionRange(input, pos, pos);
+}
